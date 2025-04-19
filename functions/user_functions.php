197c0add0b8ed6427977d3cb1b4b720ae81ac30e
Seph-0 <salinasmj16@gmail.com>
2025-04-19 14:24:53 +0800

    commit

--- functions/user_functions.php
+++ functions/user_functions.php
@@ -4,14 +4,15 @@
 
     if ($_SERVER['REQUEST_METHOD'] === 'POST' && isset($_POST['action'])) {
         try {
-            switch ($_POST['action']) {
-                case 'createUser':
-                    createUser($conn);
-                    break;
-                    break;
-                default:
-                    echo json_encode(['success' => false, 'message' => 'Unknown action']);
-                    return;
+            if ($_POST['action'] == 'createUser') {
+                createUser($conn);
+            } else if ($_POST['action'] == 'updateUser') {
+                updateUser($conn);
+            } else if ($_POST['action'] == 'deleteUser') {
+                deleteUser($conn);
+            } else {
+                echo json_encode(['success' => false, 'message' => 'Unknown action']);
+                return;
             }
 
             echo json_encode(['success' => true]);
@@ -38,6 +39,26 @@
         $stmt = $conn->prepare("INSERT INTO users (user_image, first_name, middle_name, last_name, address, sex, birthdate, user_number, email, phone, password) VALUES (?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?)");
         $stmt->bind_param("sssssssssss", $user_image, $first_name, $middle_name, $last_name, $address, $sex, $birthdate, $user_number, $email, $phone, $password);
 
+        $stmt->execute();
+        $stmt->close();
+    }
+
+    function updateUser($conn) {
+        $user_id = $_POST['user_id'];
+        $user_image = $_POST['user_image'] ?? 'default_user.jpg';
+        $first_name = $_POST['first_name'];
+        $middle_name = $_POST['middle_name'];
+        $last_name = $_POST['last_name'];
+        $address = $_POST['address'];
+        $sex = $_POST['sex'];
+        $birthdate = $_POST['birthdate'];
+        $user_number = $_POST['user_number'];
+        $email = $_POST['email'];
+        $phone = $_POST['phone'];
+        $password = password_hash($_POST['password'], PASSWORD_DEFAULT);
+        $stmt = $conn->prepare("UPDATE users SET user_image=?, first_name=?, middle_name=?, last_name=?,
+        address=?, sex=?, birthdate=?, user_number=?, email=?, phone=?, password=? WHERE user_id=?");
+        $stmt->bind_param("sssssssssssi", $user_image, $first_name, $middle_name, $last_name, $address, $sex, $birthdate, $user_number, $email, $phone, $password, $user_id);    
         $stmt->execute();
         $stmt->close();
     }
